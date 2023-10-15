a954ddc3f35cdcbdc28808c091ab1af102852d10
jeonsho <jeonsho1999@gmail.com>
2023-10-15 06:35:34 -0700

    test

--- _printf.c
+++ _printf.c
@@ -32,6 +32,7 @@ int _printf(const char *format, ...)
 					break;
 		
 				default:
+					count += _putchar('%');
                                         count += _putchar(*format);
 					break;
 			}
