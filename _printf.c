8ca15aa6ec56548c05fc86fbaba762f75760713a
jeonsho <jeonsho1999@gmail.com>
2023-10-15 07:16:46 -0700

    test

--- _printf.c
+++ _printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 	
 	va_start(argsList, format);
 	count = 0;
-	if (format == NULL || (*format == '%' && *(++format) == '\0'))
+	if (format == NULL || (*format == '%' && format[1] == '\0'))
 			return (-1);
 	while (*format != '\0')
 	{
