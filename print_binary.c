02418114bfa7a29e6ece498571870cd96003c5ba
jeonsho <jeonsho1999@gmail.com>
2023-10-16 07:02:24 -0700

    test

--- print_binary.c
+++ print_binary.c
@@ -1,41 +1,34 @@
 #include "main.h"
+void convert_and_print(unsigned int num, int *count);
 
 /**
- *print_binary - Prints an unsigned integer in binary
- *@argsList: va_list containing the unsigned integer to print
- *Return: Number of characters printed.
+ * print_bin - prints int to binary
+ * b: pointer to unsigned int
+ * Return: integer, number of characters printed
  */
-int print_binary(va_list argsList)
+
+int print_binary(va_list b)
 {
-	unsigned int num;
-	int count;
-        int binary[32];
-        int i;
-        int size;
-	if (argsList == NULL)
-	{
-		return (0);
-	}
-	count = 0;
-	num = va_arg(argsList, unsigned int);
-	if (num == 0)
-	{
-		putchar('0');
-		return (1);
-	}
+	unsigned int num = va_arg(b, unsigned int);
+	int count = 0;
 
-	size = 0;
-	while (num > 0)
-	{
-		binary[size++] = num % 2;
-		num /= 2;
-	}
-
-	for (i = size - 1; i >= 0; i--)
-	{
-		putchar(binary[i] + '0');
-		count++;
-	}
+	convert_and_print(num, &count);
 
 	return (count);
+}
+
+/**
+ * convert_and_print - converts int to binary, then prints
+ * @num: unsigned int
+ * @count: pointer to count the iteration
+ */
+
+void convert_and_print(unsigned int num, int *count)
+{
+	if (num / 2)
+		convert_and_print(num / 2, count);
+
+	_putchar((num % 2) + '0');
+
+	(*count)++;
 }
